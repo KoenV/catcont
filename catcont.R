a93ce4af38535946af7c5c649f3baead9f44d882
Koen Vanbrabant <kn.vanbrabant@gmail.com>
2016-11-30 11:16:25 +0100

    formal tests added
    added formal tests for categoric and continuous variables

--- catcont.R
+++ catcont.R
@@ -1,10 +1,21 @@
+############################################################################################################
+# Function catcon: make summary tables of categorical and continuous data with optional between group tests
+# Analyses: koen.vanbrabant@kuleuven.be
+# date: 30/11/2016
+############################################################################################################
+# required packages ---------------------------------------------------------
 library(Hmisc)
 library(pander)
 library(pROC)
-#format_pval
+# required functions ---------------------------------------------------------
+format_pval <- function(x){
+  if (x < .001) return(paste('<', '.001'))
+  else round(x, 3)   # 3 = no. of digits to round p value to if .001 < p < .250.
+}
 
-catcont = function(list_of_variables=c(),data=c(),group=c(),which.group=c(),con.test=c()){
-  #require(Hmisc)
+
+# catcon function ---------------------------------------------------------
+catcont = function(list_of_variables=c(),data=c(),group=c(),which.group=c(),formal.test=c()){
   ## list to save info on all variables (length is number of considerd variables)
   seperate_info = vector('list',length(list_of_variables))
   if (group==FALSE){
@@ -70,12 +81,45 @@ catcont = function(list_of_variables=c(),data=c(),group=c(),which.group=c(),con.
         seperate_info[[i]][5,3+ngroup] = paste0(range(data_subset[,list_of_variables[i]],na.rm=TRUE)[1],',',range(data_subset[,list_of_variables[i]],na.rm=TRUE)[2]) 
         
       }
+      if(formal.test==TRUE){
+        # overal test
+        Template_OveralTest = matrix('',nrow(TemplateMatrix_MultipleGroups_Continuous),1)
+        colnames(Template_OveralTest) = 'Overall'
+        Template_OveralTest[1,1] = 'p.val'
+        Template_OveralTest[2,1] = format_pval(kruskal.test(data[,list_of_variables[i]] ~ data[,which.group])$p.value)
+        
+        
+        Template_ContinuousTest = matrix('',nrow(TemplateMatrix_MultipleGroups_Continuous),
+                                         nlevels(data[,which.group])*(nlevels(data[,which.group])-1)/2)
+        colnames(Template_ContinuousTest) = letters[1:ncol(Template_ContinuousTest)]
+        
+        testnr=1
+        for(k in 1:(length(levels(data[,which.group]))-1)){
+          for(j in 2:length(levels(data[,which.group]))){
+            if(k<j){
+              colnames(Template_ContinuousTest)[testnr] = paste0('[',levels(data[,which.group])[k],' vs.',
+                                                                 levels(data[,which.group])[j],']')
+              
+              Template_ContinuousTest[1,testnr] = 'AUC (p.val)'
+              
+              data_subset = subset(data,data[,which.group] %in% levels(data[,which.group])[c(k,j)]) # take subset on which.group levels
+              
+              Template_ContinuousTest[2,testnr] = paste0(round(auc(droplevels(data_subset[,which.group]), data_subset[,list_of_variables[i]]),2),
+                                                         '(',
+                                                         format_pval(wilcox.test(data_subset[,list_of_variables[i]] ~ droplevels(data_subset[,which.group]))$p.value),
+                                                         ')')
+              testnr =  testnr + 1
+            }
+          }
+        }
+        seperate_info[[i]] = cbind(seperate_info[[i]],Template_OveralTest,Template_ContinuousTest)
+      }
     }
     ## Discrete --------------------------------------------------------------
       if(is.factor(data[,list_of_variables[i]]) | is.character(data[,list_of_variables[i]])){
-        TemplateMatrix_MultipleGroups_Discrete = matrix(c(''),nlevels(data[,list_of_variables[i]])+1,nlevels(data[,which.group])+3)
+        TemplateMatrix_MultipleGroups_Discrete = matrix(c(''),nlevels(data[,list_of_variables[i]])+2,nlevels(data[,which.group])+3)
         TemplateMatrix_MultipleGroups_Discrete[1,1] = paste0('**',ifelse(label(data[,list_of_variables[i]])=='',list_of_variables[i],label(data[,list_of_variables[i]])),'**')
-        TemplateMatrix_MultipleGroups_Discrete[2:(nlevels(data[,list_of_variables[i]])+1),2] = 'n/N (%)'
+        TemplateMatrix_MultipleGroups_Discrete[3:(nlevels(data[,list_of_variables[i]])+2),2] = 'n/N (%)'
         colnames(TemplateMatrix_MultipleGroups_Discrete) = c('variable','statistic','total',c(levels(data[,which.group])))
         seperate_info[[i]] = TemplateMatrix_MultipleGroups_Discrete
         
@@ -86,13 +130,64 @@ catcont = function(list_of_variables=c(),data=c(),group=c(),which.group=c(),con.
           for(level in 1:nlevels(data[,list_of_variables[i]])){
             data[,'count_dummy'] = ifelse(data[,list_of_variables[i]]==levels(data[,list_of_variables[i]])[level],1,0)
             
-            seperate_info[[i]][level+1,1] = levels(data_subset[,list_of_variables[i]])[level]
+            seperate_info[[i]][level+2,1] = levels(data_subset[,list_of_variables[i]])[level]
             
-            seperate_info[[i]][level+1,3] = paste0(sum(data[,'count_dummy']),'/',nrow(data),'(',round((sum(data[,'count_dummy'])/nrow(data))*100,2),'%',')')
+            seperate_info[[i]][level+2,3] = paste0(sum(data[,'count_dummy']),'/',nrow(data),'(',round((sum(data[,'count_dummy'])/nrow(data))*100,2),'%',')')
             
             data_subset[,'count_dummy'] = ifelse(data_subset[,list_of_variables[i]]==levels(data_subset[,list_of_variables[i]])[level],1,0)
-            seperate_info[[i]][level+1,3+ngroup] = paste0(sum(data_subset[,'count_dummy']),'/',nrow(data_subset),'(',round((sum(data_subset[,'count_dummy'])/nrow(data_subset))*100,2),'%',')')
+            seperate_info[[i]][level+2,3+ngroup] = paste0(sum(data_subset[,'count_dummy']),'/',nrow(data_subset),'(',round((sum(data_subset[,'count_dummy'])/nrow(data_subset))*100,2),'%',')')
           }
+        }
+        if(formal.test==TRUE){
+          # overall test -- based on Fisher exact test.
+          Template_OveralTest_discrete = matrix('',nrow(TemplateMatrix_MultipleGroups_Discrete),1)
+          colnames(Template_OveralTest_discrete) = 'Overall'
+          Template_OveralTest_discrete[1,1] = 'p.val'
+          Template_OveralTest_discrete[2,1] = format_pval((fisher.test(table(data[,which.group],data[,list_of_variables[i]]),alternative = "two.sided",conf.level=.95))$p.value)
+          
+          Template_DiscreteTest = matrix('',nrow(TemplateMatrix_MultipleGroups_Discrete),
+                                         nlevels(data[,which.group])*(nlevels(data[,which.group])-1)/2)
+          colnames(Template_DiscreteTest) = letters[1:ncol(Template_DiscreteTest)] # circumvent error when giving correct names in each pairwaise comparison
+          if(nlevels(data[,list_of_variables[i]])==2){
+            testnr=1
+            for(k in 1:(length(levels(data[,which.group]))-1)){
+              for(j in 2:length(levels(data[,which.group]))){
+                if(k<j){
+                  colnames(Template_DiscreteTest)[testnr] = paste0('[',levels(data[,which.group])[k],' vs.',
+                                                                   levels(data[,which.group])[j],']')
+                  
+                  Template_DiscreteTest[1,testnr] = '95% CI (p.val)'
+                  
+                  data_subset = subset(data,data[,which.group] %in% levels(data[,which.group])[c(k,j)]) # take subset on which.group levels
+                  hold.CI = round((prop.test(table(droplevels(data_subset[,which.group]),data_subset[,list_of_variables[i]]), correct=FALSE,conf.level=.95))$conf.int,2)
+                  Template_DiscreteTest[2,testnr] = paste0(paste0('[',hold.CI[1],',',hold.CI[2],']'),
+                                                           '(',
+                                                           format_pval((fisher.test(table(data_subset[,which.group],data_subset[,list_of_variables[i]]),alternative = "two.sided",conf.level=.95))$p.value),
+                                                           ')')
+                  testnr =  testnr + 1
+                }
+              }
+            }
+          } else{
+            testnr=1
+            for(k in 1:(length(levels(data[,which.group]))-1)){
+              for(j in 2:length(levels(data[,which.group]))){
+                if(k<j){
+                  colnames(Template_DiscreteTest)[testnr] = paste0('[',levels(data[,which.group])[k],' vs.',
+                                                                   levels(data[,which.group])[j],']')
+                  
+                  Template_DiscreteTest[1,testnr] = '(p.val)'
+                  
+                  data_subset = subset(data,data[,which.group] %in% levels(data[,which.group])[c(k,j)]) # take subset on which.group levels
+                  Template_DiscreteTest[2,testnr] = paste0('(',
+                                                           format_pval((fisher.test(table(data_subset[,which.group],data_subset[,list_of_variables[i]]),alternative = "two.sided",conf.level=.95))$p.value),
+                                                           ')')
+                  testnr =  testnr + 1
+                }
+              }
+            }
+          }
+          seperate_info[[i]] = cbind(seperate_info[[i]],Template_OveralTest_discrete,Template_DiscreteTest)
         }
       }
     }
