547908c58ea48cab11e06b94ef8e754e31509ae9
Koen Vanbrabant <kn.vanbrabant@gmail.com>
2017-03-15 16:34:36 +0100

    Add files via upload

--- catcont.R
+++ catcont.R
@@ -12,79 +12,151 @@ if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
 }
 
 # catcon function ---------------------------------------------------------
-catcont = function(list_of_variables=c(),data=c(),group=c(),which.group=c(),formal.test=c(),digits=2,caption='Summary Table'){
-  full_data = data # keep an untouched part of the data to re-use in the NA subsetting
-  # format p-values function (run in function environment)
-  format_pval <- function(x){
-    if (x < .001) return(paste('<', '.001'))
-    else round(x, 3)   # 3 = no. of digits to round p value to if .001 < p < .250.
-  }
+catcont = function(list_of_variables=c(),data=c(),group=c(),which.group=c(),
+    formal.test=c(),digits=2,caption='Summary Table'){
+    
+    # keep an untouched part of the data to re-use in the NA subsetting
+    full_data = data 
   
+    # format p-values function (run in function environment)
+    format_pval <- function(x){
+        if (x < .001) return(paste('<', '.001'))
+        else round(x, 3)
+    }
+  
+    
   ## list to save info on all variables (length is number of considerd variables)
   seperate_info = vector('list',length(list_of_variables))
-  if (group==FALSE){
-    for(i in 1:length(list_of_variables)){
-      data = full_data[!is.na(full_data[,list_of_variables[i]]),] # only work with non-missing data on the variable of interest
-      # group is none ---------------------------------------
-      if(is.numeric(data[,list_of_variables[i]]) | is.integer(data[,list_of_variables[i]])){
-        ## Continuous --------------------------------------------------------------
-        TemplateMatrix_OneGroup_Continuous = matrix(c(''),5,3)
-        TemplateMatrix_OneGroup_Continuous[,2] = c('','N','mean (std)','median (IQR)','range')
-        colnames(TemplateMatrix_OneGroup_Continuous) = c('variable','statistic','')
-        seperate_info[[i]] = TemplateMatrix_OneGroup_Continuous
-        # calculate the statistics 
-        seperate_info[[i]][1,1] = paste0('**',ifelse(label(data[,list_of_variables[i]])=='',list_of_variables[i],label(data[,list_of_variables[i]])),'**')
-        seperate_info[[i]][2,3] = nrow(data)
-        seperate_info[[i]][3,3] = paste0(round(mean(data[,list_of_variables[i]],na.rm=TRUE),digits),'(',round(sd(data[,list_of_variables[i]],na.rm=TRUE),digits),')') 
-        seperate_info[[i]][4,3] = paste0(round(median(data[,list_of_variables[i]],na.rm = TRUE),digits),'(',paste0(round(quantile(data[,list_of_variables[i]],c(.25),na.rm=TRUE),digits),';'
-                                                                                        ,round(quantile(data[,list_of_variables[i]],c(.75),na.rm=TRUE),digits)),')')
-        seperate_info[[i]][5,3] = paste0(round(range(data[,list_of_variables[i]],na.rm=TRUE)[1],digits),',',round(range(data[,list_of_variables[i]],na.rm=TRUE)[2],digits)) 
-      }    
+  
+    if (group==FALSE){
+    
+        for(i in 1:length(list_of_variables)){
+        
+        # only work with non-missing data on the variable of interest
+        data = full_data[!is.na(full_data[,list_of_variables[i]]),] 
       
-      if(is.factor(data[,list_of_variables[i]]) | is.character(data[,list_of_variables[i]]) ){
-        ## Discrete --------------------------------------------------------------
-        TemplateMatrix_OneGroup_Discrete = matrix(c(''),nlevels(data[,list_of_variables[i]])+1,3)
-        TemplateMatrix_OneGroup_Discrete[1,1] = paste0('**',ifelse(label(data[,list_of_variables[i]])=='',list_of_variables[i],label(data[,list_of_variables[i]])),'**')
-        TemplateMatrix_OneGroup_Discrete[2:(nlevels(data[,list_of_variables[i]])+1),2] = 'n/N (%)'
-        colnames(TemplateMatrix_OneGroup_Discrete) = c('variable','statistic','')
+        # group is none ---------------------------------------
+        if(is.numeric(data[,list_of_variables[i]]) | is.integer(data[,list_of_variables[i]])){
+            ## Continuous --------------------------------------------------------------
+            TemplateMatrix_OneGroup_Continuous = matrix(c(''),5,3)
+            
+            TemplateMatrix_OneGroup_Continuous[,2] = c('','N','mean (std)',
+                'median (IQR)','range')
+            
+            colnames(TemplateMatrix_OneGroup_Continuous) = c('variable','statistic','')
+            
+            seperate_info[[i]] = TemplateMatrix_OneGroup_Continuous
         
-        seperate_info[[i]] = TemplateMatrix_OneGroup_Discrete
+            # calculate the statistics 
+            seperate_info[[i]][1,1] = paste0('**',ifelse(label(data[,list_of_variables[i]])=='',
+                list_of_variables[i],label(data[,list_of_variables[i]])),'**')
+            
+            seperate_info[[i]][2,3] = nrow(data)
+            
+            seperate_info[[i]][3,3] = paste0(round(mean(data[,
+                list_of_variables[i]],na.rm=TRUE),digits),'(',round(sd(data[,
+                    list_of_variables[i]],na.rm=TRUE),digits),')') 
+            
+            seperate_info[[i]][4,3] = paste0(round(median(data[,list_of_variables[i]],
+                na.rm = TRUE),digits),'(',paste0(round(quantile(data[,list_of_variables[i]],c(.25),
+                    na.rm=TRUE),digits),';',round(quantile(data[,list_of_variables[i]],
+                        c(.75),na.rm=TRUE),digits)),')')
+            
+            
+            seperate_info[[i]][5,3] = paste0(round(range(data[,
+                list_of_variables[i]],na.rm=TRUE)[1],digits),',',
+                round(range(data[,list_of_variables[i]],na.rm=TRUE)[2],digits)) 
+        }    
+      
+        if(is.factor(data[,list_of_variables[i]]) | is.character(data[,list_of_variables[i]]) ){
+            ## Discrete --------------------------------------------------------------
+            TemplateMatrix_OneGroup_Discrete = matrix(c(''),
+                nlevels(data[,list_of_variables[i]])+1,3)
+            
+            TemplateMatrix_OneGroup_Discrete[1,1] = paste0('**',
+                ifelse(label(data[,list_of_variables[i]])=='',list_of_variables[i],
+                    label(data[,list_of_variables[i]])),'**')
+            
+            TemplateMatrix_OneGroup_Discrete[2:(nlevels(data[,
+                list_of_variables[i]])+1),2] = 'n/N (%)'
+            
+            colnames(TemplateMatrix_OneGroup_Discrete) = c('variable','statistic','')
+        
+            seperate_info[[i]] = TemplateMatrix_OneGroup_Discrete
+            
         for(level in 1:nlevels(data[,list_of_variables[i]])){
-          data[,'count_dummy'] = ifelse(data[,list_of_variables[i]]==levels(data[,list_of_variables[i]])[level],1,0)
-          seperate_info[[i]][level+1,1] = levels(data[,list_of_variables[i]])[level]
-          seperate_info[[i]][level+1,3] = paste0(sum(data[,'count_dummy']),'/',nrow(data),'(',(sum(data[,'count_dummy'])/nrow(data))*100,'%',')')
-        }
-      }
-    }
-    full_table = do.call("rbind", seperate_info)
-    return(knitr::kable(full_table))
-  }
-  if (group==TRUE){
-    for(i in 1:length(list_of_variables)){
-      data = full_data[!is.na(full_data[,list_of_variables[i]]),] # only work with non-missing data on the variable of interest
-      ## Continuous --------------------------------------------------------------
-      if(is.numeric(data[,list_of_variables[i]]) | is.integer(data[,list_of_variables[i]])){
-        TemplateMatrix_MultipleGroups_Continuous = matrix(c(''),5,nlevels(data[,which.group])+3)
-        colnames(TemplateMatrix_MultipleGroups_Continuous) = c('variable','statistic','total',levels(data[,which.group]))
-        TemplateMatrix_MultipleGroups_Continuous[1,1] = paste0('**',ifelse(label(data[,list_of_variables[i]])=='',list_of_variables[i],label(data[,list_of_variables[i]])),'**')
-        TemplateMatrix_MultipleGroups_Continuous[2:5,2] = c('N','mean (std)','median (IQR)','range')
-        seperate_info[[i]]=TemplateMatrix_MultipleGroups_Continuous
-        # total information
-        seperate_info[[i]][2,3] = nrow(data)
-        seperate_info[[i]][3,3] = paste0(round(mean(data[,list_of_variables[i]],na.rm=TRUE),2),'(',round(sd(data[,list_of_variables[i]],na.rm=TRUE),digits),')') 
-        seperate_info[[i]][4,3] = paste0(round(median(data[,list_of_variables[i]]),digits),'(',paste0(round(quantile(data[,list_of_variables[i]],c(.25),na.rm=TRUE),digits),';',
-                                                                                        round(quantile(data[,list_of_variables[i]],c(.75),na.rm=TRUE),digits)),')')
-        
-        seperate_info[[i]][5,3] = paste0(round(range(data[,list_of_variables[i]],na.rm=TRUE)[1],digits),',',round(range(data[,list_of_variables[i]],na.rm=TRUE)[2],digits)) 
-        # seperate group information
-        for(ngroup in 1:nlevels(data[,which.group])){
-          data_subset = subset(data,data[,which.group]==levels(data[,which.group])[ngroup]) # take subset on which.group levels
+            
+          data[,'count_dummy'] = ifelse(data[,list_of_variables[i]]==levels(data[,
+              list_of_variables[i]])[level],1,0)
           
-          seperate_info[[i]][2,3+ngroup] = length(data_subset[,list_of_variables[i]])
-          seperate_info[[i]][3,3+ngroup] = paste0(round(mean(data_subset[,list_of_variables[i]],na.rm=TRUE),2),'(',round(sd(data_subset[,list_of_variables[i]],na.rm=TRUE),digits),')') 
-          seperate_info[[i]][4,3+ngroup] = paste0(round(median(data_subset[,list_of_variables[i]]),digits),'(',paste0(round(quantile(data_subset[,list_of_variables[i]],c(.25),na.rm=TRUE),digits),',',
+          seperate_info[[i]][level+1,1] = levels(data[,list_of_variables[i]])[level]
+          
+          seperate_info[[i]][level+1,3] = paste0(sum(data[,'count_dummy']),'/',
+              nrow(data),'(',round((sum(data[,'count_dummy'])/nrow(data))*100,2),'%',')')
+                }
+            }
+        }
+        
+        full_table = do.call("rbind", seperate_info)
+    
+        return(knitr::kable(full_table))
+    }
+  
+  
+    if (group==TRUE){
+        
+        for(i in 1:length(list_of_variables)){
+            
+            # only work with non-missing data on the variable of interest
+            data = full_data[!is.na(full_data[,list_of_variables[i]]),] 
+            
+        ## Continuous --------------------------------------------------------------
+            if(is.numeric(data[,list_of_variables[i]]) | is.integer(data[,list_of_variables[i]])){
+                
+            TemplateMatrix_MultipleGroups_Continuous = matrix(c(''),5,
+                nlevels(data[,which.group])+3)
+            
+            colnames(TemplateMatrix_MultipleGroups_Continuous) = 
+                c('variable','statistic','total',levels(data[,which.group]))
+            
+            TemplateMatrix_MultipleGroups_Continuous[1,1] = paste0('**',
+                ifelse(label(data[,list_of_variables[i]])=='',list_of_variables[i],
+                    label(data[,list_of_variables[i]])),'**')
+            
+            TemplateMatrix_MultipleGroups_Continuous[2:5,2] = 
+                c('N','mean (std)','median (IQR)','range')
+            
+            seperate_info[[i]]=TemplateMatrix_MultipleGroups_Continuous
+            
+            # total information
+            
+            seperate_info[[i]][2,3] = nrow(data)
+            
+            seperate_info[[i]][3,3] = paste0(round(mean(data[,list_of_variables[i]],
+                na.rm=TRUE),2),'(',round(sd(data[,list_of_variables[i]],na.rm=TRUE),digits),')') 
+            
+            seperate_info[[i]][4,3] = paste0(round(median(data[,list_of_variables[i]]),
+                digits),'(',paste0(round(quantile(data[,list_of_variables[i]],c(.25),
+                    na.rm=TRUE),digits),';',round(quantile(data[,list_of_variables[i]],
+                        c(.75),na.rm=TRUE),digits)),')')
+        
+            seperate_info[[i]][5,3] = paste0(round(range(data[,list_of_variables[i]],
+                na.rm=TRUE)[1],digits),',',round(range(data[,list_of_variables[i]],
+                    na.rm=TRUE)[2],digits)) 
+            
+            # seperate group information
+            for(ngroup in 1:nlevels(data[,which.group])){
+            
+                data_subset = subset(data,data[,which.group]==levels(data[,which.group])[ngroup]) # take subset on which.group levels
+          
+                seperate_info[[i]][2,3+ngroup] = length(data_subset[,list_of_variables[i]])
+                
+                seperate_info[[i]][3,3+ngroup] = paste0(round(mean(data_subset[,list_of_variables[i]],na.rm=TRUE),2),'(',round(sd(data_subset[,list_of_variables[i]],na.rm=TRUE),digits),')') 
+                
+                seperate_info[[i]][4,3+ngroup] = paste0(round(median(data_subset[,list_of_variables[i]]),digits),'(',paste0(round(quantile(data_subset[,list_of_variables[i]],c(.25),na.rm=TRUE),digits),',',
                                                                                                         round(quantile(data_subset[,list_of_variables[i]],c(.75),na.rm=TRUE),digits)),')')
-          seperate_info[[i]][5,3+ngroup] = paste0(round(range(data_subset[,list_of_variables[i]],na.rm=TRUE)[1],digits),',',round(range(data_subset[,list_of_variables[i]],na.rm=TRUE)[2],digits)) 
+                
+                seperate_info[[i]][5,3+ngroup] = paste0(round(range(data_subset[,list_of_variables[i]],na.rm=TRUE)[1],digits),',',round(range(data_subset[,list_of_variables[i]],na.rm=TRUE)[2],digits)) 
           
         }
         if(formal.test==TRUE){
